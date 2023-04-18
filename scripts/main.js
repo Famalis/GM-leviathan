8555b42d33e505245d1aeab4bcf4802778695650
Sergio <sergio@copro.no>
2023-04-18 13:59:53 +0200

    pkg

--- scripts/main.js
+++ scripts/main.js
@@ -1,14 +1,26 @@
-console.log("Hello World! This code runs immediately when the file is loaded.")
+const print = (...data) => {
+  console.log("GM-Leviathan tools", data);
+};
 
 Hooks.on("init", function () {
-  console.log(
+  print(
     "This code runs once the Foundry VTT software begins its initialization workflow."
   );
 });
 
 Hooks.on("ready", function () {
-  console.log(
+  print(
     "This code runs once core initialization is ready and game data is available."
   );
 });
+
+Hooks.on("refreshToken", function (token) {
+  print("Refresh token", token);
+});
+
+//const myHookId = Hooks.on("updateActor", this.onUpdateActor.bind(this));
+//Hooks.off('updateActor', this.onUpdateActor);
+//async function onUpdateActor(actor, data, options, userId) {
+//do my things here
+//}
 
