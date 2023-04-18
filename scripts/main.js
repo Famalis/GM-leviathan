d17c256c69244f7d16e96b6a6b9928642e782b6f
Sergio <sergio@copro.no>
2023-04-18 13:13:42 +0200

    pkg

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,14 @@
-console.debug("My awesome module");
+console.log("Hello World! This code runs immediately when the file is loaded.")
+
+Hooks.on("init", function () {
+  console.log(
+    "This code runs once the Foundry VTT software begins its initialization workflow."
+  );
+});
+
+Hooks.on("ready", function () {
+  console.log(
+    "This code runs once core initialization is ready and game data is available."
+  );
+});
+
